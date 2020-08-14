0a79fb84784eeee9d8205dbf7947c80abcf2fc97
unknown <6110110072@psu.ac.th>
2020-08-15 04:01:09 +0700

    part7.3

--- card-game/src/WordCard.js
+++ card-game/src/WordCard.js
@@ -35,7 +35,7 @@ export default function WordCard(props) {
         alert("try agian!");
 
         state.attempt += 1;
-        setState({ ...state, guess: "", attempt: state.attempt+1});
+        setState({ ...state, guess: "", attempt: state.attempt});
       }
     }
     temp = state.attempt;
