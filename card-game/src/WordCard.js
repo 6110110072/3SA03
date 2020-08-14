907f007b223101cf9a381024a74811bbbf0e0145
unknown <6110110072@psu.ac.th>
2020-08-14 22:15:40 +0700

    part 5

--- card-game/src/WordCard.js
+++ card-game/src/WordCard.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import CharacterCard from './CharacterCard';
+import React from "react";
+import CharacterCard from "./CharacterCard";
 
-export default function WordCard(props){
- return (
- <div>
-{ Array.from(props.value).map((c, i) => <CharacterCard value={c} key={i}/>) }
- </div>
- );
+export default function WordCard(props) {
+  const activationHandler = (c) => {
+    console.log(`${c} has been activated.`);
+  };
+
+//   <CharacterCard value={c} key={i} activationHandler={activationHandler} />;
+
+  return (
+    <div>
+      {Array.from(props.value).map((c, i) => (
+        <CharacterCard
+          value={c}
+          key={i}
+          activationHandler={activationHandler}
+        />
+      ))}
+    </div>
+  );
 }
