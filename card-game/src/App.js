7a0fa01d025750170ab796072abe1fbe06a9cf3a
unknown <6110110072@psu.ac.th>
2020-08-14 21:47:28 +0700

    part 4

--- card-game/src/App.js
+++ card-game/src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import WordCard from './WordCard';
+// import CharacterCard from './CharacterCard';
+
+const word = "Hello";
 function App() {
  return (
  <div>
- Hello World
+ {
+//  Array.from(word).map((c, i) => <CharacterCard value={c} key={i}/>)
+ <WordCard value="hello"/>
+ }
  </div>
  );
 }
+
 export default App;
